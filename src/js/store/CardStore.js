b64e3ff5f4a28ccff13e422b6dfc3a344a70906b
fengjb <18794758986@163.com>
2018-04-27 15:52:00 +0800

    uptate cardstore

--- src/js/store/CardStore.js
+++ src/js/store/CardStore.js
@@ -1,6 +1,7 @@
-import {toJson} from '../utils/utils.js'
-import {CARD} from '../def/state.js'
-import { getNomalCmd } from '../utils/cardStoreDep.js'
+import { toJson } from '../utils/utils.js'
+import { CARD } from '../def/state.js'
+import { processDetail } from '../utils/cardStoreDep.js'
+
 const UNCOVER = 1000
 const SPECIAL = 1001
 export default {
@@ -17,14 +18,14 @@ export default {
     lastUpdateTime: 0
   },
   mutations: {
-    updateRefreshDuration (state) {
+    updateRefreshDuration(state) {
       let inow = window.performance.now()
       if (state.lastUpdateTime > 0) {
         state.averageUpdateDuration = inow - state.lastUpdateTime
       }
       state.lastUpdateTime = inow
     },
-    processStat (state, data,type) {
+    processStat(state, data, type) {
       let stat = null
 
       if (data) {
@@ -35,132 +36,44 @@ export default {
         }
       }
 
-      data.type === 'vehicle'? state.vstate = stat : state.sstate = stat
+      data.type === 'vehicle' ? state.vstate = stat : state.sstate = stat
     }
   },
   actions: {
-    cardUpdatePos ({state, dispatch, commit}, data) {
+    cardUpdatePos({ state, dispatch, commit }, data) {
       dispatch('cardMove', data)
       commit('updateRefreshDuration')
     },
-    cardMove ({dispatch}, data) {
+    cardMove({ dispatch }, data) {
       if (!data) return
       data = toJson(data)
       dispatch('processVehicleData', data.v)
       dispatch('processStaffData', data.s)
     },
-    async processVehicleData ({state, dispatch,commit}, data) {
+    processVehicleData({ state, dispatch, commit }, data) {
       if (!data) {
         return
       }
       data.stat.type = 'vehicle'
-      commit('processStat',data.stat)
+      commit('processStat', data.stat)
       state.overview.vehicle = state.vstate ? state.vstate.sum : 0
-      state.vcards = await dispatch('processDetail', data.detail)
+      state.vcards = processDetail(state, data.detail)
     },
-    async processStaffData ({state, dispatch,commit}, data) {
+    processStaffData({ state, dispatch, commit }, data) {
       if (!data) return
       data.stat.type = 'staff'
-      commit('processStat',data.stat)
+      commit('processStat', data.stat)
       state.overview.staff = state.sstate ? state.sstate.sum : 0
 
-      state.scards = await dispatch('processDetail', data.detail)
+      state.scards = processDetail(state, data.detail)
     },
-    async processDetail ({state, dispatch}, data) {
-      let xmap = new Map()
-      if (data) {
-        for (let i = 0, len = data.length; i < len; i++) {
-          let card = data[i]
-          let cardID = card[CARD.card_id]
-          card = await dispatch('addFields', {
-            cardID: cardID,
-            card: card
-          })
-
-          if (this.state.user.deptID === 0 || card[CARD.dept_id] === this.state.user.deptID) { // 全局 或 对应部门的详情
-            xmap.set(cardID, card)
-            dispatch('showCard', {
-              cardID: cardID,
-              card: card
-            })
-          }
-        }
-      }
-
-      return xmap
-    },
-    async addFields ({state, dispatch}, data) {
-      let cardID = data.cardID
+    showCard({ dispatch }, data) {
       let card = data.card
-      let cardTypeID = -1
-      let cardBindedObjectID = ''
-      let cardTypeInfo = await this.dispatch('metaStore/getCardTypeInfo', cardID)
-
-      if (cardTypeInfo) {
-        cardTypeID = cardTypeInfo.card_type_id
-        let bindedObject = await this.dispatch('metaStore/getCardBindObjectInfo', cardID)
-        if (bindedObject) {
-          cardBindedObjectID = bindedObject.name
-        }
-      } else {
-        console.warn(`Can NOT find cardTypeInfo for ${cardID}`)
-      }
-
-      card && card.splice(1, 0, cardTypeID, cardBindedObjectID)
-      return card
-    },
-    /**
-     * 根据卡 推送过来的状态类型，获得对应的 cmd 指令
-     * @param {*} 车:(非覆盖区域/运动/静止/停车场) 与 (丢失信号)
-     * @param {*} 人: 接收 与 没接收到
-     */
-    async getCmdByState ({state, dispatch}, data) {
-      let cmd = 'POSITION'
-      let card = data.card
-      let cardID = data.cardID
-      let areas = this.state.metaStore.data.area
-      let areatype = this.state.metaStore.data.area_type
-      let areaID = card[CARD.area_id]
-      let area = areas && areas.get(areaID)
-      let areaTypeID = area && Number(area.area_type_id)
-      let areaTypeName = areaTypeID && areatype && areatype.get(areaTypeID).area_type_id
-      areaTypeName = parseInt(areaTypeName, 10)
-      let cardTypeName = state.bindcard.typeInfoName
-      await this.dispatch('metaStore/getCardTypeName', cardID)
-
-      let cardstate = card[CARD.state_biz] // 卡的业务状态 接收/没接收信号
-      if (cardstate === 1024) { // 接收不到信号,车辆变灰
-        if (cardTypeName === 'staff') {
-          cmd = 'NOSIGNAL' // 卡类型为人员时,存入无信号人员卡缓冲区
-          if (!state.nosignalscars.has(cardID)) {
-            state.nosignalscars.set(cardID, card)
-          }
-        } else if (cardTypeName === 'vehicle') {
-          if (areaTypeID < 1000) { // 正常区域时，会出现无信号状态
-            cmd = 'NOSIGNAL'
-          } else { // 非覆盖区域 or 胶轮车硐室 的情况
-            cmd = getNomalCmd(state,{
-              areaTypeName: areaTypeName,
-              cardID: cardID
-            })
-          }
-        }
-      } else {
-        cmd = getNomalCmd(state,{
-          areaTypeName: areaTypeName,
-          cardID: cardID
-        })
-      }
-
-      return cmd
-    },
-    async showCard ({dispatch}, data) {
-      let card = data.card
-      let cmd = await dispatch('getCmdByState', data)
-      this.dispatch('olMapCardLayer/informMapUpdateCard', {
-        cmd: cmd,
-        card: card
-      })
+      let cmd = getCmdByState(xdata, data)
+      // this.dispatch('olMapCardLayer/informMapUpdateCard', {
+      //   cmd: cmd,
+      //   card: card
+      // })
     }
   }
 }
--- src/js/utils/cardStoreDep.js
+++ src/js/utils/cardStoreDep.js
@@ -1,3 +1,5 @@
+import {getCardBindObjectInfo,getCardTypeInfo} from './metaStoreDep.js'
+
 function getNomalCmd (state, data) {
     let cmd = 'POSITION'
     let areaTypeName = data.areaTypeName
@@ -21,4 +23,87 @@ function getNomalCmd (state, data) {
     return cmd
 }
 
-export { getNomalCmd }
+function getCmdByState (xdata, data) {
+    let cmd = 'POSITION'
+    let card = data.card
+    let cardID = data.cardID
+    let areas = xdata.state.metaStore.data.area
+    let areatype = xdata.state.metaStore.data.area_type
+    let areaID = card[CARD.area_id]
+    let area = areas && areas.get(areaID)
+    let areaTypeID = area && Number(area.area_type_id)
+    let areaTypeName = areaTypeID && areatype && areatype.get(areaTypeID).area_type_id
+    areaTypeName = parseInt(areaTypeName, 10)
+    let cardTypeName = getCardTypeName(state,cardID)
+
+    let cardstate = card[CARD.state_biz] // 卡的业务状态 接收/没接收信号
+    if (cardstate === 1024) { // 接收不到信号,车辆变灰
+      if (cardTypeName === 'staff') {
+        cmd = 'NOSIGNAL' // 卡类型为人员时,存入无信号人员卡缓冲区
+        if (!state.nosignalscars.has(cardID)) {
+          state.nosignalscars.set(cardID, card)
+        }
+      } else if (cardTypeName === 'vehicle') {
+        if (areaTypeID < 1000) { // 正常区域时，会出现无信号状态
+          cmd = 'NOSIGNAL'
+        } else { // 非覆盖区域 or 胶轮车硐室 的情况
+          cmd = getNomalCmd(state,{
+            areaTypeName: areaTypeName,
+            cardID: cardID
+          })
+        }
+      }
+    } else {
+      cmd = getNomalCmd(state,{
+        areaTypeName: areaTypeName,
+        cardID: cardID
+      })
+    }
+
+    return cmd
+  }
+
+  function addFields (state, data) {
+    let cardID = data.cardID
+    let card = data.card
+    let cardTypeID = -1
+    let cardBindedObjectID = ''
+    let cardTypeInfo = getCardTypeInfo(state,cardID)
+
+    if (cardTypeInfo) {
+      cardTypeID = cardTypeInfo.card_type_id
+      let bindedObject = getCardBindObjectInfo(state,cardID)
+
+      if (bindedObject) {
+        cardBindedObjectID = bindedObject.name
+      }
+    } else {
+      console.warn(`Can NOT find cardTypeInfo for ${cardID}`)
+    }
+
+    card && card.splice(1, 0, cardTypeID, cardBindedObjectID)
+    return card
+  }
+
+  function processDetail (state, data) {
+    let xmap = new Map()
+    if (data) {
+      for (let i = 0, len = data.length; i < len; i++) {
+        let card = data[i]
+        let cardID = card[CARD.card_id]
+        card = addFields(state,{cardID: cardID,card: card})
+
+        if (this.state.user.deptID === 0 || card[CARD.dept_id] === this.state.user.deptID) { // 全局 或 对应部门的详情
+          xmap.set(cardID, card)
+          // dispatch('showCard', {
+          //   cardID: cardID,
+          //   card: card
+          // })
+        }
+      }
+    }
+
+    return xmap
+  }
+
+export { getNomalCmd ,getCmdByState,addFields,processDetail}
